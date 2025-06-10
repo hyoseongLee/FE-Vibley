adf604a5ea98196a17f1f4e643e4b6d314c6d8f6
Eunmi Lee <eunoia0235@gmail.com>
2025-06-11 03:58:44 +0900

    feat: Header 컴포넌트 추가

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from './Button';
+import { Link } from 'react-router-dom';
+import { RiUserLine } from 'react-icons/ri';
+
+const Header: React.FC = () => {
+  return (
+    <header className='fixed top-0 left-0 w-full flex items-center justify-between px-[60px] py-3 bg-white shadow-sm'>
+      <Link to='/main' className='text-logo font-shrikhand text-black'>
+        Vibely
+      </Link>
+
+      <Button className='px-2 py-2'>
+        <RiUserLine size={20} />
+      </Button>
+    </header>
+  );
+};
+
+export default Header;
