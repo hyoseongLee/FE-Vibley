e2c209367f04d2adb78220695a7780b605cd2491
hyoseong <hyoseong123@naver.com>
2025-06-12 04:54:03 +0900

    feat: 메인페이지 구현현

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,20 @@
+import AIChatC from "../components/ai/AIChat";
+import ShowAlbums from "../components/music/AlbumList";
+
 const Main = () => {
-  return <div>메인</div>;
+  
+  return (
+    <div className="flex items-start justify-center gap-40 p-4">
+      <div className="flex flex-col gap-1">
+        <ShowAlbums type="new" title="최신 발매 음악" />
+        <ShowAlbums type="liked-album" title="좋아요한 앨범" />
+        <ShowAlbums type="liked-playlist" title="나의 플레이리스트" />
+      </div>
+      <div className="mt-10">
+        <AIChatC />
+      </div>
+    </div>
+  );
 };
 
 export default Main;
