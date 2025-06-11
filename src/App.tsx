88bd208709dfa7eba713e3d568a4559e71585e30
Eunmi Lee <eunoia0235@gmail.com>
2025-06-12 03:15:52 +0900

    feat: 불필요한 부분 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
 import AppRoutes from './routes/AppRoutes';
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
-  return <AppRoutes />;
   return (
     <BrowserRouter>
       <AppRoutes />
